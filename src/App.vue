4f9a39875428196c53e7cd42a2cc7967b0a45899
Ilija Radosavljevic <45182003+ilija02@users.noreply.github.com>
2024-06-02 21:42:48 +0200

    add button for switching language

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <router-view />
+  <LanguageSwitch />
 </template>
 
 <script>
+import LanguageSwitch from '@/components/LanguageSwitch.vue';
+export default {
+  name: "App",
+  components: {
+    LanguageSwitch
+  },
+};
 </script>
---
+++ src/components/LanguageSwitch.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="fixed bottom-4 left-4 transform z-50 ">
+        <div v-if="showOptions">
+            <button @click="setLanguage('en')"
+                class="block w-24 bg-white text-gray-900 px-4 py-2 rounded-full shadow-md focus:outline-none mb-1">English</button>
+            <button @click="setLanguage('sr')"
+                class="block w-24 bg-white text-gray-900 px-4 py-2 rounded-full shadow-md focus:outline-none mb-1">Serbian</button>
+        </div>
+        <button @click="toggleOptions"
+            class="bg-primary-500 text-white px-4 py-2 rounded-full  focus:outline-none shadow-sm shadow-gray-900">
+            <fa icon="flag" class="mr-1" />
+            {{ this.$i18n.locale }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showOptions: false,
+        };
+    },
+    methods: {
+        toggleOptions() {
+            this.showOptions = !this.showOptions;
+        },
+        setLanguage(lang) {
+            this.$i18n.locale = lang;
+            this.showOptions = false;
+        },
+    },
+};
+</script>
+
+<style scoped></style>
