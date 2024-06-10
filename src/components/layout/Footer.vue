2639396721f461ce843b8d1a4646048fe7bb4ec3
Ilija Radosavljevic <45182003+ilija02@users.noreply.github.com>
2024-06-10 13:28:58 +0200

    fix footer on mobile

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,8 +1,10 @@
 <template>
-    <footer class="py-2 w-full z-10">
-        <p class="text-center text-sm italic">&copy; 2024, Ilija Radosavljević, Zlatko Golubović, {{
+    <footer class="py-2 flex items-center justify-center w-full z-10">
+
+        <p class="text-center w-1/3 md:w-full text-sm italic ">&copy; 2024, Ilija Radosavljević, Zlatko Golubović, {{
             $t('footer_text')
-            }}</p>
+        }}</p>
+        <div class="h-12 md:hidden"></div>
     </footer>
 </template>
 
